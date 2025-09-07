eb5c1328288809ed4c7e4f77a3d6e53d0418f0b1
ASSIGNMENT1 <rajarchita399@gmail.com>
2025-09-07 21:56:24 +0530

    init commit

---
+++ src/SumOfTwo.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class SumOfTwo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number");
+        int a = sc.nextInt();
+        System.out.println("enter the number");
+        int b = sc.nextInt();
+        int sum =a+b;
+        if (sum>100){
+            System.out.println("the sum is greater than 100");
+        }else{
+            System.out.println("the sum is not greater than 100");
+        }
+    }
+}
