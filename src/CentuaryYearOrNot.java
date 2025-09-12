9a51fc22b8f052161278960334eb5467dc556062
rchitaraj <140628397+rchitaraj@users.noreply.github.com>
2025-09-12 12:42:52 +0530

    Update CentuaryYearOrNot.java

--- src/CentuaryYearOrNot.java
+++ src/CentuaryYearOrNot.java
@@ -5,7 +5,7 @@ public class CentuaryYearOrNot {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter the year");
         int year= sc.nextInt();
-        if(year%400==0){
+        if(year%400==0 && year%100==0){
             System.out.println(year+" The given year is a leap year");
         }else{
             System.out.println(year+" The given year is not a leap year");
